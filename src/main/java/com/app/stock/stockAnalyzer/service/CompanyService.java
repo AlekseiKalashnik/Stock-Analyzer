69316f8f46698db12f82c248b60754b42534018f
AlekseiKalashnik <id50538996@gmail.com>
2023-06-27 09:31:33 +0300

    try with 50 companies

--- src/main/java/com/app/stock/stockAnalyzer/service/CompanyService.java
+++ src/main/java/com/app/stock/stockAnalyzer/service/CompanyService.java
@@ -28,7 +28,7 @@ public class CompanyService {
             List<Company> companies = iexApiClient.getCompaniesData()
                     .join()
                     .stream()
-                    .map(this::convertToCompany).limit(25)
+                    .map(this::convertToCompany).limit(50)
                     .toList();
             return companyRepository.saveAll(companies);
         });

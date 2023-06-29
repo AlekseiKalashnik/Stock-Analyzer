e0687a21c1efb2931ca0d9eacbaed468217b8a92
AlekseiKalashnik <id50538996@gmail.com>
2023-06-29 13:26:46 +0300

    unable oauth2

--- src/main/java/com/app/stock/stockAnalyzer/controller/AuthRestController.java
+++ src/main/java/com/app/stock/stockAnalyzer/controller/AuthRestController.java
@@ -1,14 +1,14 @@
 package com.app.stock.stockAnalyzer.controller;
 
-import com.app.stock.stockAnalyzer.dto.UserDTO;
-import com.app.stock.stockAnalyzer.service.UserService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import com.app.stock.stockAnalyzer.dto.UserDTO;
+//import com.app.stock.stockAnalyzer.service.UserService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.oauth2.core.user.OAuth2User;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
 
 //@Slf4j
 //@RestController

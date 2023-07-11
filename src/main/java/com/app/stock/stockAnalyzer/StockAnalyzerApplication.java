a2b8ae527f751bb8deef4d88b268dd4cd49f57ef
AlekseiKalashnik <id50538996@gmail.com>
2023-07-11 22:20:52 +0300

    changes before consumer transfer

--- src/main/java/com/app/stock/stockAnalyzer/StockAnalyzerApplication.java
+++ src/main/java/com/app/stock/stockAnalyzer/StockAnalyzerApplication.java
@@ -3,14 +3,13 @@ package com.app.stock.stockAnalyzer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableAsync
 @EnableKafka
 @EnableScheduling
+@SpringBootApplication
 public class StockAnalyzerApplication {
 
     public static void main(String[] args) {
